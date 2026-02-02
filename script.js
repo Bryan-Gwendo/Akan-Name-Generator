a76102e5040c72c373bf209630015fde76de56ed
Bryan Gwendo <bryangwendo314@gmail.com>
2026-02-02 14:08:49 +0300

    Declared gender variable

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function generateAkan() {
     let day = parseInt(document.getElementById("day").value);
     let month = parseInt(document.getElementById("month").value);
     let year = parseInt(document.getElementById("year").value);
+
+    
+    let gender = document.querySelector('input[name="gender"]:checked');
