e4256783a79be5db3fbc4b6e86b37a9884b298ea
Bryan Gwendo <bryangwendo314@gmail.com>
2026-02-02 14:08:03 +0300

    Introduced generateAkan function and declared day,year and month variables

---
+++ script.js
@@ -0,0 +1,5 @@
+function generateAkan() {
+
+    let day = parseInt(document.getElementById("day").value);
+    let month = parseInt(document.getElementById("month").value);
+    let year = parseInt(document.getElementById("year").value);
